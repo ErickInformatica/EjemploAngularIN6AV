4201ab9e4608b811c9fd14ca0a0650d213cfad15
ErickInformatica <36520780+ErickInformatica@users.noreply.github.com>
2021-04-29 14:47:32 -0600

    Clase 29-04

--- src/app/servicios/usuario.service.ts
+++ src/app/servicios/usuario.service.ts
@@ -10,6 +10,8 @@ import { Usuario } from '../modelos/usuario.model';
 export class UsuarioService {
   public ruta: String;
   public headersVariable = new HttpHeaders().set('Content-Type', 'application/json');
+  public token;
+  public identidad;
   constructor(public _http: HttpClient) {
     this.ruta = GLOBAL.url;
   }
@@ -25,4 +27,38 @@ export class UsuarioService {
 
     return this._http.get(this.ruta + 'obtenerUsuarios', {headers: this.headersVariable})
   }
+
+  login(usuario, getToken = null): Observable<any>{
+    if(getToken != null){
+      usuario.getToken = getToken;
+    }
+    let params = JSON.stringify(usuario);
+
+    return this._http.post(this.ruta + 'login', params, { headers: this.headersVariable });
+
+  }
+
+  obtenerIdentidad(){
+    var identidad2 = JSON.parse(localStorage.getItem('identidad'));
+    if(identidad2 != 'undefined'){
+      this.identidad = identidad2;
+    }else{
+      this.identidad = null;
+    }
+
+    return this.identidad;
+  }
+
+  obtenerToken(){
+    var token2 = localStorage.getItem('token');
+    if(token2 != 'undefined'){
+      this.token = token2;
+    }else{
+      this.token = null;
+    }
+
+    return this.token;
+  }
+
+
 }
